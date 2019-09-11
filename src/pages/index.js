31c781b72a77fafcaf6eaaed9df96f15d7f89c0d
Vitor Dino <vitor@kunstdesign.com.br>
2019-09-10 22:02:10 -0300

    :sparkles: add Button on home to see all articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,53 @@
 import {graphql} from 'gatsby'
 import React, {Fragment} from 'react'
+import styled from 'styled-components'
 import {mapProps, compose} from 'recompose'
 import flattenBlogPostInfo from '../fragments/BlogPostInfo'
+import {colors} from '../constants'
 import {withLayout} from '../components/Layout'
 import MetaTags from '../components/MetaTags'
 import Container from '../components/Container'
+import Link from '../components/StylableLink'
 import {CardRow} from '../components/CardGrid'
 import Navbar from '../components/Navbar'
 import TitleBar from '../components/TitleBar'
 import HomeHero from '../components/HomeHero'
 import PostCard from '../components/PostCard'
+
+const Button = styled(Link)`
+	display: flex;
+	align-items: center;
+	position: relative;
+	justify-content: space-between;
+	padding: 1.75rem 2rem;
+	background: ${colors.white};
+	color: ${colors.base};
+	font-weight: 600;
+	text-decoration: none;
+	color: ${colors.base88};
+	&:after {
+		content: '→';
+		font-size: 2em;
+		font-weight: 300;
+	}
+	&:before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		box-shadow: 0 0.75rem 2rem -0.375rem rgba(0,0,0,0.1);
+		opacity: 0;
+		transition: all .6s cubic-bezier(0.165, 0.84, 0.44, 1);
+	}
+	&:hover, &:focus, &:active{
+		z-index: 1;
+		&:before {
+			opacity: 1;
+		}
+	}
+`
 
 const PageComponent = ({posts: [hero, ...posts]}) => (
 	<Fragment>
@@ -34,6 +72,7 @@ const PageComponent = ({posts: [hero, ...posts]}) => (
 						/>
 					))}
 				</CardRow>
+				<Button to='/materias'>Ver todas as matérias</Button>
 			</Container>
 		</main>
 	</Fragment>
